db9c066bced018088b7284492ddae53df35425da
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-08 01:21:08 +0200

    init commit

---
+++ api/models/user_model.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, text, Integer
+from sqlalchemy.dialects.postgresql import TIMESTAMP
+from api.db.base_class import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
