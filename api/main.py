b5d8b41913a214f193827714f4e8efb093dec215
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-08 01:26:50 +0200

    allow all origins and add test endpoint

---
+++ api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.controller import user_controller
+
+app = FastAPI()
+app.include_router(user_controller.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/')
+def hello():
+    return "Hack NASA with HTML only"
