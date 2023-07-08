76dc3e8c77a2ad381089b8e7f802826db9f75dc6
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-08 19:27:08 +0200

    refactor database configuration all models has been moved in base module

--- api/db/database_config.py
+++ api/db/database_config.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from api.config.env_config import settings as cfg
-from api.models import user_model, role_model
+from api.db.base import Base
 
 SQLALCHEMY_DB_URL = f'postgresql://{cfg.db_username}:{cfg.db_password}@{cfg.db_hostname}:{cfg.db_port}/{cfg.db_name}'
 engine = create_engine(SQLALCHEMY_DB_URL)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-user_model.Base.metadata.create_all(bind=engine)
-role_model.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 
 def get_db():
