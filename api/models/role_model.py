a5edeb6b8c39bf9af84d20ab6c1812a83ffb0ff8
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-13 10:27:43 +0200

    remove description from role

--- api/models/role_model.py
+++ api/models/role_model.py
@@ -9,6 +9,5 @@ class Role(Base):
 
     id = Column(Integer, primary_key=True, nullable=False, index=True)
     name = Column(String, nullable=False, unique=True)
-    description = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     users = relationship('User', secondary='user_role', back_populates='roles')
