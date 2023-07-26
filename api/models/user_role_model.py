33cd984afb5d06fcbf467fadd9201db23f68ac4c
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-26 16:31:59 +0200

    refactor associate table user_role for cascading delete

--- api/models/user_role_model.py
+++ api/models/user_role_model.py
@@ -4,7 +4,7 @@ from api.db.base import Base
 user_role = Table(
     'user_role',
     Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('role_id', Integer, ForeignKey('roles.id'))
+    Column('user_id', Integer, ForeignKey('users.id', ondelete="CASCADE")),
+    Column('role_id', Integer, ForeignKey('roles.id', ondelete="CASCADE"))
 )
 
