c3f00ba763d89917bab594ca01275963fff09573
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-27 13:31:15 +0200

    add delete role functionality

--- tests/controller/test_role_controller.py
+++ tests/controller/test_role_controller.py
@@ -98,3 +98,44 @@ def test_update_role_already_exist_error_message(client, test_roles, name):
     response = client.put("/api/roles", json=role)
     error = response.json().get("detail")
     assert error == f"Role with name: {name} already exists"
+
+
+def test_delete_role_success(client, test_roles):
+    response = client.delete(f"/api/roles/{test_roles[2].id}")
+    assert response.status_code == 204
+
+
+def test_delete_role_not_found(client):
+    response = client.delete("/api/roles/9999")
+    assert response.status_code == 404
+
+
+def test_delete_role_not_found_error_message(client):
+    response = client.delete("/api/roles/9999")
+    error = response.json().get("detail")
+    assert error == "Role with id: 9999 not found"
+
+
+@pytest.mark.parametrize("id", [
+    "abc",
+    "%id=1",
+    "&1",
+    "&id=1",
+    1.1
+])
+def test_delete_role_wrong_id(client, id):
+    response = client.delete(f"/api/roles/{id}")
+    assert response.status_code == 422
+
+
+@pytest.mark.parametrize("id", [
+    "abc",
+    "%id=1",
+    "&1",
+    "&id=1",
+    1.1
+])
+def test_delete_role_wrong_id_error_message(client, id):
+    response = client.delete(f"/api/roles/{id}")
+    error = response.json().get("detail")
+    assert error == "value is not a valid integer"
