1a9ea55bf04b8f0c3dedc63e720d48e68c93d229
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-30 00:12:49 +0200

    :🔒️ Fix security issues.: secure all role endpoints

--- api/controller/role_controller.py
+++ api/controller/role_controller.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from api.db.database_config import get_db
 from api.exception.exception import EntityNotFoundException, EntityAlreadyExistsException, BadRequestException
+from api.models.user_model import User
 from api.schemas.role_schemas import RoleOut, RoleCreate, RoleUpdate
+from api.security.oauth2 import get_principal
 from api.service.role_service import RoleService
 
 router = APIRouter(tags=['Roles'])
@@ -11,7 +13,7 @@ role_service = RoleService()
 
 
 @router.post('/api/roles', status_code=status.HTTP_201_CREATED, response_model=RoleOut)
-def save_role(role_create: RoleCreate, db: Session = Depends(get_db)):
+def save_role(role_create: RoleCreate, db: Session = Depends(get_db), principal: User = Depends(get_principal)):
     try:
         return role_service.save(role_create, db)
     except EntityAlreadyExistsException as err:
@@ -21,7 +23,7 @@ def save_role(role_create: RoleCreate, db: Session = Depends(get_db)):
 
 
 @router.get('/api/roles/name', status_code=status.HTTP_200_OK, response_model=RoleOut)
-def get_role_by_name(name: str, db: Session = Depends(get_db)):
+def get_role_by_name(name: str, db: Session = Depends(get_db), principal: User = Depends(get_principal)):
     try:
         return role_service.get_role_by_name(name, db)
     except EntityNotFoundException as err:
@@ -29,12 +31,12 @@ def get_role_by_name(name: str, db: Session = Depends(get_db)):
 
 
 @router.get("/api/roles", status_code=status.HTTP_200_OK, response_model=List[RoleOut])
-def get_all_roles(db: Session = Depends(get_db)):
+def get_all_roles(db: Session = Depends(get_db), principal: User = Depends(get_principal)):
     return role_service.get_all(db)
 
 
 @router.put("/api/roles", status_code=status.HTTP_200_OK, response_model=RoleOut)
-def update_role(role: RoleUpdate, db: Session = Depends(get_db)):
+def update_role(role: RoleUpdate, db: Session = Depends(get_db), principal: User = Depends(get_principal)):
     try:
         return role_service.update_role(role, db)
     except EntityNotFoundException as err:
@@ -44,7 +46,7 @@ def update_role(role: RoleUpdate, db: Session = Depends(get_db)):
 
 
 @router.delete("/api/roles/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_role(id: int, db: Session = Depends(get_db)):
+def delete_role(id: int, db: Session = Depends(get_db), principal: User = Depends(get_principal)):
     try:
         role_service.delete_role(id, db)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
