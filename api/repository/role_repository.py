a8c0623cf44ec45811566aed0b73393fc3375323
Vitalii Sili <vitaliisili@yahoo.com>
2023-07-29 14:51:34 +0200

    DOCS: :books: add documentation for get_role_by_name in RoleRepository class

--- api/repository/role_repository.py
+++ api/repository/role_repository.py
@@ -7,10 +7,10 @@ class RoleRepository(ReadWriteRepository):
     def get_role_by_name(self, name: str, db: Session) -> Role:
         """ Retrieves a role from the database based on its name.
         Parameters:
-        name (str): The name of the role to retrieve.
-        db (Session): The database session object.
+            name (str): The name of the role to retrieve.
+            db (Session): The database session object.
         Returns:
-        Role: The role object retrieved from the database, if found.
+            Role: The role object retrieved from the database, if found.
         """
         role = db.query(Role).filter_by(name=name).first()
         return role
