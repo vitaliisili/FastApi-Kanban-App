44e421a4f5dd33381076055b5722ea2d7e6a0ea2
Vitalii Sili <vitaliisili@yahoo.com>
2023-08-01 15:43:34 +0200

    ♻️ Refactor code. Change test implementation, now each user has its own pytest fixture

--- tests/controller/test_user_controller.py
+++ tests/controller/test_user_controller.py
@@ -20,15 +20,15 @@ def test_get_all_user_unauthorized_user_error_message(client, test_users):
     assert error == "Not authenticated"
 
 
-def test_get_user_by_id_success(authorized_admin_client, test_users):
-    response = authorized_admin_client.get(f"/api/users/{test_users[0].id}")
+def test_get_user_by_id_success(authorized_admin_client, test_simple_user):
+    response = authorized_admin_client.get(f"/api/users/{test_simple_user.id}")
     user = UserOut(**response.json())
     assert response.status_code == 200
-    assert user.email == test_users[0].email
+    assert user.email == test_simple_user.email
 
 
-def test_get_user_by_id_unauthorized_user(client, test_users):
-    response = client.get(f"/api/users/{test_users[0].id}")
+def test_get_user_by_id_unauthorized_user(client, test_simple_user):
+    response = client.get(f"/api/users/{test_simple_user.id}")
     assert response.status_code == 401
 
 
@@ -60,35 +60,35 @@ def test_get_user_by_id_not_valid_error_message(authorized_admin_client):
     assert error == "value is not a valid integer"
 
 
-def test_update_user_success_update_email(authorized_admin_client, test_users):
+def test_update_user_success_update_email(authorized_admin_client, test_simple_user):
     data = {
-        "id": test_users[0].id,
+        "id": test_simple_user.id,
         "email": "new_email@email.com",
-        "first_name": test_users[0].first_name,
-        "last_name": test_users[0].last_name,
+        "first_name": test_simple_user.first_name,
+        "last_name": test_simple_user.last_name,
         "roles": [
             {
-                "id": test_users[0].roles[0].id,
-                "name": test_users[0].roles[0].name
+                "id": test_simple_user.roles[0].id,
+                "name": test_simple_user.roles[0].name
             }
         ]
     }
     response = authorized_admin_client.put("/api/users", json=data)
     user = UserOut(**response.json())
     assert response.status_code == 200
-    assert user.email == test_users[0].email
+    assert user.email == test_simple_user.email
 
 
-def test_update_user_unauthorized_user(client, test_users):
+def test_update_user_unauthorized_user(client, test_simple_user):
     data = {
-        "id": test_users[0].id,
+        "id": test_simple_user.id,
         "email": "new_email@email.com",
-        "first_name": test_users[0].first_name,
-        "last_name": test_users[0].last_name,
+        "first_name": test_simple_user.first_name,
+        "last_name": test_simple_user.last_name,
         "roles": [
             {
-                "id": test_users[0].roles[0].id,
-                "name": test_users[0].roles[0].name
+                "id": test_simple_user.roles[0].id,
+                "name": test_simple_user.roles[0].name
             }
         ]
     }
@@ -96,50 +96,50 @@ def test_update_user_unauthorized_user(client, test_users):
     assert response.status_code == 401
 
 
-def test_update_user_success_update_first_name(authorized_admin_client, test_users):
+def test_update_user_success_update_first_name(authorized_admin_client, test_simple_user):
     data = {
-        "id": test_users[0].id,
-        "email": test_users[0].email,
+        "id": test_simple_user.id,
+        "email": test_simple_user.email,
         "first_name": "New First Name",
-        "last_name": test_users[0].last_name,
+        "last_name": test_simple_user.last_name,
         "roles": [
             {
-                "id": test_users[0].roles[0].id,
-                "name": test_users[0].roles[0].name
+                "id": test_simple_user.roles[0].id,
+                "name": test_simple_user.roles[0].name
             }
         ]
     }
     response = authorized_admin_client.put("/api/users", json=data)
     user = UserOut(**response.json())
     assert response.status_code == 200
-    assert user.first_name == test_users[0].first_name
+    assert user.first_name == test_simple_user.first_name
 
 
-def test_update_user_success_update_last_name(authorized_admin_client, test_users):
+def test_update_user_success_update_last_name(authorized_admin_client, test_simple_user):
     data = {
-        "id": test_users[0].id,
-        "email": test_users[0].email,
-        "first_name": test_users[0].first_name,
+        "id": test_simple_user.id,
+        "email": test_simple_user.email,
+        "first_name": test_simple_user.first_name,
         "last_name": "New Last Name",
         "roles": [
             {
-                "id": test_users[0].roles[0].id,
-                "name": test_users[0].roles[0].name
+                "id": test_simple_user.roles[0].id,
+                "name": test_simple_user.roles[0].name
             }
         ]
     }
     response = authorized_admin_client.put("/api/users", json=data)
     user = UserOut(**response.json())
     assert response.status_code == 200
-    assert user.last_name == test_users[0].last_name
+    assert user.last_name == test_simple_user.last_name
 
 
-def test_update_user_success_add_admin_role(authorized_admin_client, test_users):
+def test_update_user_success_add_admin_role(authorized_admin_client, test_simple_user):
     data = {
-        "id": test_users[0].id,
-        "email": test_users[0].email,
-        "first_name": test_users[0].first_name,
-        "last_name": test_users[0].last_name,
+        "id": test_simple_user.id,
+        "email": test_simple_user.email,
+        "first_name": test_simple_user.first_name,
+        "last_name": test_simple_user.last_name,
         "roles": [
             {
                 "id": 1,
@@ -155,8 +155,8 @@ def test_update_user_success_add_admin_role(authorized_admin_client, test_users)
     response = authorized_admin_client.put("/api/users", json=data)
     user = UserOut(**response.json())
     assert response.status_code == 200
-    assert len(user.roles) == len(test_users[0].roles)
-    assert user.roles[1].name == test_users[0].roles[1].name
+    assert len(user.roles) == len(test_simple_user.roles)
+    assert user.roles[1].name == test_simple_user.roles[1].name
 
 
 def test_update_user_not_found(authorized_admin_client):
@@ -194,10 +194,10 @@ def test_update_user_not_found_error_message(authorized_admin_client):
     assert error == "User with id: 99999 not found"
 
 
-def test_update_user_already_exist(authorized_admin_client, test_users):
+def test_update_user_already_exist(authorized_admin_client, test_admin_user):
     data = {
         "id": 1,
-        "email": test_users[1].email,
+        "email": test_admin_user.email,
         "first_name": "first_name",
         "last_name": "last_name",
         "roles": [
@@ -212,10 +212,10 @@ def test_update_user_already_exist(authorized_admin_client, test_users):
     assert response.status_code == 409
 
 
-def test_update_user_already_exist_error_message(authorized_admin_client, test_users):
+def test_update_user_already_exist_error_message(authorized_admin_client, test_admin_user):
     data = {
         "id": 1,
-        "email": test_users[1].email,
+        "email": test_admin_user.email,
         "first_name": "first_name",
         "last_name": "last_name",
         "roles": [
@@ -228,16 +228,16 @@ def test_update_user_already_exist_error_message(authorized_admin_client, test_u
 
     response = authorized_admin_client.put("/api/users", json=data)
     error = response.json().get("detail")
-    assert error == f"User with email: {test_users[1].email} already exists"
+    assert error == f"User with email: {test_admin_user.email} already exists"
 
 
-def test_delete_user_success(authorized_admin_client, test_users):
-    response = authorized_admin_client.delete(f"/api/users/{test_users[0].id}")
+def test_delete_user_success(authorized_admin_client, test_simple_user):
+    response = authorized_admin_client.delete(f"/api/users/{test_simple_user.id}")
     assert response.status_code == 204
 
 
-def test_delete_user_unauthorized(client, test_users):
-    response = client.delete(f"/api/users/{test_users[0].id}")
+def test_delete_user_unauthorized(client, test_simple_user):
+    response = client.delete(f"/api/users/{test_simple_user.id}")
     assert response.status_code == 401
 
 
