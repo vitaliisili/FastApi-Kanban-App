9cee1b28a6249cf2f488eee3e01cb9a7a428c8d3
Vitalii Sili <vitaliisili@yahoo.com>
2023-08-05 16:42:47 +0200

    🏷️ Add or update types. Add correct types

--- api/repository/user_repository.py
+++ api/repository/user_repository.py
@@ -1,3 +1,5 @@
+from typing import Type, List
+
 from sqlalchemy.orm import Session
 from api.models.role_model import Role
 from api.models.user_model import User
@@ -6,7 +8,7 @@ from api.repository.read_write_repository import ReadWriteRepository
 
 
 class UserRepository(ReadWriteRepository):
-    def get_user_by_email(self, email: str, db: Session) -> User:
+    def get_user_by_email(self, email: str, db: Session) -> Type[User]:  # noqa
         """ Retrieves a user from the database based on their email.
          Parameters:
             email (str): The email of the user to retrieve.
@@ -14,10 +16,10 @@ class UserRepository(ReadWriteRepository):
          Returns:
             User: The user object retrieved from the database, if found.
         """
-        user: User = db.query(User).filter_by(email=email).first()
+        user: Type[User] = db.query(User).filter_by(email=email).first()
         return user
 
-    def update(self, user_update: UserUpdate, db: Session) -> User:
+    def update(self, user_update: UserUpdate, db: Session) -> Type[User]:
         """ Updates a user in the database with the provided user_update data.
         Parameters:
             user_update (UserUpdate): The user update object containing the new user data.
@@ -25,13 +27,13 @@ class UserRepository(ReadWriteRepository):
         Returns:
             User: The updated user object.
         """
-        user = db.query(User).filter_by(id=user_update.id).first()
+        user: Type[User] = db.query(User).filter_by(id=user_update.id).first()
 
         for field, value in user_update.dict(exclude={"roles"}).items():
             if value is not None:
                 setattr(user, field, value)
 
-        roles = db.query(Role).filter(Role.id.in_([role.id for role in user_update.roles])).all()
+        roles: List[Type[Role]] = db.query(Role).filter(Role.id.in_([role.id for role in user_update.roles])).all()
         user.roles = roles
 
         db.commit()
