43efa759704ac06b6455ae065f86c23553b647df
Vitalii Sili <vitaliisili@yahoo.com>
2023-08-05 16:44:12 +0200

    🏷️ Add or update types. Add correct types ✨ Introduce new features. Add update workspace endpoint

--- api/controller/workspace_controller.py
+++ api/controller/workspace_controller.py
@@ -1,11 +1,11 @@
-from typing import List, Annotated
+from typing import List, Annotated, Type
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from api.db.database_config import get_db
-from api.exception.exception import EntityNotFoundException, EntityAlreadyExistsException
+from api.exception.exception import EntityNotFoundException, EntityAlreadyExistsException, PermissionDeniedException
 from api.models.user_model import User
 from api.models.workspace_model import Workspace
-from api.schemas.workspace_schemas import WorkspaceOut, WorkspaceCreate
+from api.schemas.workspace_schemas import WorkspaceOut, WorkspaceCreate, WorkspaceUpdate
 from api.security.jwt_config import get_principal
 from api.service.workspace_service import WorkspaceService
 
@@ -41,7 +41,7 @@ def get_all_workspaces(db: Annotated[Session, Depends(get_db)]):
             dependencies=[Depends(get_principal)])
 def get_all_workspaces_by_owner_id(owner_id: int, db: Session = Depends(get_db)):
     try:
-        workspaces: List[Workspace] = workspace_service.get_all_workspaces_by_owner_id(owner_id, db)
+        workspaces: List[Type[Workspace]] = workspace_service.get_all_workspaces_by_owner_id(owner_id, db)
         return workspaces
     except EntityNotFoundException as err:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
@@ -53,7 +53,20 @@ def get_all_workspaces_by_owner_id(owner_id: int, db: Session = Depends(get_db))
             dependencies=[Depends(get_principal)])
 def get_workspace_by_id(id: int, db: Session = Depends(get_db)):
     try:
-        workspace: Workspace = workspace_service.get_workspace_by_id(id, db)
+        workspace: Type[Workspace] = workspace_service.get_workspace_by_id(id, db)
         return workspace
     except EntityNotFoundException as err:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
+
+
+@router.put("/api/workspaces", status_code=status.HTTP_200_OK, response_model=WorkspaceOut)
+def update_workspace(workspace: WorkspaceUpdate,
+                     db: Session = Depends(get_db),
+                     principal: User = Depends(get_principal)):
+    try:
+        workspace: Type[Workspace] = workspace_service.update_workspace(workspace, db, principal)
+        return workspace
+    except EntityNotFoundException as err:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
+    except PermissionDeniedException as err:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(err))
